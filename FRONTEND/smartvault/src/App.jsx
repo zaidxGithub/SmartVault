612339d614d0bf87c47ce533b15717f825856079
zaidxGithub <zaidxcodes@gmail.com>
2025-08-07 22:05:20 +0530

    feat: initialize smartvault project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Created App component with basic structure and styles.
    - Added global styles in index.css and App.css.
    - Included React logo SVG asset.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ FRONTEND/smartvault/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h2 className='bg-red-100'> vite project main page where all the subprojects will be imported
+
+    </h2>
+    </>
+  )
+}
+
+export default App
