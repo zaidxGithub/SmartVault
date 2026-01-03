8783ce37df71299d84c526c3b4edb99409124ed4
zaidxGithub <zaidxcodes@gmail.com>
2026-01-03 23:05:47 +0530

    Fixed firebase.js imports

--- BACKEND/src/firebase/firebase.js
+++ BACKEND/src/firebase/firebase.js
@@ -1,13 +1,20 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
-import path from "path";
-import { fileURLToPath} from "url";
-const _filename=fileURLToPath(import.meta.url);
-const _dirname=   path.dirname(_filename);
+import dotenv from "dotenv";
+dotenv.config();
+const serviceAccount = {
+ 
+  type: process.env.FB_TYPE,
+  project_id:process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID,
+  private_key: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FB_CLIENT_CERT_URL,
+};
 
-const serviceAccount = JSON.parse(
-  readFileSync(path.join(_dirname,"firebase-service-account.json" ),"utf-8") 
-);
 
 if (!admin.apps.length) {
   admin.initializeApp({
